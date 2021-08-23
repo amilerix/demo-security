50c457613f5ca33b2e699596cf5c7370a8b2fda0
amilerix <88525627+amilerix@users.noreply.github.com>
2021-08-23 17:14:51 +0800

    initial

---
+++ src/main/java/com/example/demosecurity/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,67 @@
+package com.example.demosecurity.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.demosecurity.security.ApplicationUserRole.ADMIN;
+import static com.example.demosecurity.security.ApplicationUserRole.ADMIN_TRAINEE;
+import static com.example.demosecurity.security.ApplicationUserRole.STUDENT;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        "annabanana",
+                        passwordEncoder.encode("password"),
+                        STUDENT.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "marites",
+                        passwordEncoder.encode("password"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "sincha",
+                        passwordEncoder.encode("password"),
+                        ADMIN_TRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
+    }
+
+}
